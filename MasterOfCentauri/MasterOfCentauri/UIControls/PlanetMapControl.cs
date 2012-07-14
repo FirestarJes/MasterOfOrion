3ea368825262758a8a4747dbdceaafdb4d33745b
Jesper Blad Jensen <deldy@deldysoft.dk>
2012-07-14 10:19:40 +0200

    Basic mockup of planet view

--- MasterOfCentauri/MasterOfCentauri/UIControls/PlanetMapControl.cs
+++ MasterOfCentauri/MasterOfCentauri/UIControls/PlanetMapControl.cs
@@ -1,29 +1,38 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using System.Diagnostics;
+using DigitalRune.Mathematics.Algebra;
 using Microsoft.Xna.Framework.Graphics;
 using DigitalRune.Game.UI.Controls;
 using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework;
-using DigitalRune.Game.UI.Rendering;
 
 namespace MasterOfCentauri.UIControls
 {
-    class PlanetMapControl : DigitalRune.Game.UI.Controls.ContentControl
+    class PlanetMapControl : ContentControl
     {
-        SpriteBatch _spritebatch;
-        private SpriteFont arialFont;
-        private Texture2D sun;
-        private ContentManager _content;
-        Texture2D _black;
+        Texture2D _sun;
+        Texture2D _ring;
+        Texture2D _planet;
+        ContentManager _content;
         UIControl _sunControl;
-        Panel panel;
+        UIControl _ringControl;
+        UIControl _ringControl2;
+        UIControl _ringControl3;
+        UIControl _ringControl4;
+        UIControl _planet1Control;
+        UIControl _planet2Control;
+        UIControl _planet3Control;
+        UIControl _planet4Control;
+        Canvas _panel;
+        private float radius1 = 700f;
+        private float radius2 = 700f * 0.8f;
+        private float radius3 = 700f * 0.6f;
+        private float radius4 = 700f * 0.4f;
+        private float degrees = 45f;
 
         public PlanetMapControl(IServiceProvider services)
         {
             Name = "PlanetMapControl";
-            _spritebatch = (SpriteBatch)services.GetService(typeof(SpriteBatch));
             _content = ((ContentManager)services.GetService(typeof(ContentManager)));
             Background = Color.Black;
             ClipContent = true;
@@ -31,18 +40,65 @@ namespace MasterOfCentauri.UIControls
 
         protected override void OnLoad()
         {
-            arialFont = _content.Load<SpriteFont>(@"Fonts\Spritefont1");
-            sun = _content.Load<Texture2D>(@"PlanetView\Sun");
+            _sun = _content.Load<Texture2D>(@"PlanetView\Sun");
+            _ring = _content.Load<Texture2D>(@"PlanetView\PlanetRing");
+            _planet = _content.Load<Texture2D>(@"PlanetView\Planet1");
 
-            panel = new Canvas { HorizontalAlignment = DigitalRune.Game.UI.HorizontalAlignment.Stretch, VerticalAlignment = DigitalRune.Game.UI.VerticalAlignment.Stretch };
-            _sunControl = new Image() { Texture = sun, X = -sun.Width / 2, Y = -sun.Height / 2 };
-            Content = panel;
-            panel.Children.Add(_sunControl);
+
+            _panel = new Canvas { HorizontalAlignment = DigitalRune.Game.UI.HorizontalAlignment.Stretch, VerticalAlignment = DigitalRune.Game.UI.VerticalAlignment.Stretch };
+            _sunControl = new Image() { Texture = _sun, X = -_sun.Width / 2, Y = -_sun.Height / 2 };
+            _ringControl = new Image() { Texture = _ring };
+            _ringControl2 = new Image() { Texture = _ring, RenderScale = new Vector2F(0.8f)  };
+            _ringControl3 = new Image() { Texture = _ring, RenderScale = new Vector2F(0.6f) };
+            _ringControl4 = new Image() { Texture = _ring, RenderScale = new Vector2F(0.4f) };
+            _planet1Control = new Image() { Texture = _planet, X = radius1, Y = radius1, Opacity = 1 };
+            _planet2Control = new Image() { Texture = _planet, X = radius2, Y = radius2, Opacity = 1 };
+            _planet3Control = new Image() { Texture = _planet, X = radius3, Y = radius3, Opacity = 1 };
+
+            _planet4Control = new Image() { Texture = _planet, X = GetX(degrees, radius4), Y = GetY(degrees, radius4), Opacity = 1 };
+
+            Content = _panel;
+            _panel.Children.Add(_sunControl);
+            _panel.Children.Add(_ringControl);
+            _panel.Children.Add(_ringControl2);
+            _panel.Children.Add(_ringControl3);
+            _panel.Children.Add(_ringControl4);
+            _panel.Children.Add(_planet1Control);
+            _panel.Children.Add(_planet2Control);
+            _panel.Children.Add(_planet3Control);
+            _panel.Children.Add(_planet4Control);
+
+
             base.OnLoad();
+        }
+
+        private float GetY(float degrees, float radius4)
+        {
+            return (float)Math.Cos(MathHelper.ToRadians(degrees)) * radius4 - _planet.Height / 2;
+        }
+
+        private float GetX(float degrees, float radius4)
+        {
+            return (float)Math.Sin(MathHelper.ToRadians(degrees)) * radius4 - _planet.Width / 2;
         }
 
         protected override void OnUpdate(TimeSpan deltaTime)
         {
+            if (_planet1Control.IsMouseOver)
+            {
+                Debug.WriteLine("SHOW DIALOG");
+            }
+
+            //degrees += 0.1f;
+            _planet4Control.X = GetX(degrees + 10, radius4);
+            _planet4Control.Y = GetY(degrees + 10, radius4);
+            _planet3Control.X = GetX(degrees - 10, radius3);
+            _planet3Control.Y = GetY(degrees - 10, radius3);
+            _planet2Control.X = GetX(degrees + 5, radius2);
+            _planet2Control.Y = GetY(degrees + 5, radius2);
+            _planet1Control.X = GetX(degrees, radius1);
+            _planet1Control.Y = GetY(degrees, radius1);
+
             base.OnUpdate(deltaTime);
         }
 
