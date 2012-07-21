862422d759e1ffa83b01ffa0fa9de155b6fb39c0
Jesper Blad Jensen <deldy@deldysoft.dk>
2012-07-21 11:41:54 +0200

    BuildingCells is not in the center of the planet map

--- MasterOfCentauri/MasterOfCentauri/UIControls/PlanetMapControl.cs
+++ MasterOfCentauri/MasterOfCentauri/UIControls/PlanetMapControl.cs
@@ -11,18 +11,17 @@ namespace MasterOfCentauri.UIControls
 {
     public class PlanetMapControl : CustomRenderingContentControl, IConsoleCommandHost
     {
-        private readonly IServiceProvider _services;
-        private ContentController _content;
-        private SpriteBatch _spritebatch;
-        private SpriteFont _font;
-        private Texture2D _black;
-        private Texture2D _testTexture;
+        private readonly ContentController _content;
+        private readonly SpriteBatch _spritebatch;
+        private readonly SpriteFont _font;
+        private readonly Texture2D _black;
+        private readonly Texture2D _testTexture;
+        private static int buildingCellSize = 128;
 
         public PlanetMapViewModel ViewData { get; set; }
 
         public PlanetMapControl(IServiceProvider services)
         {
-            _services = services;
             _content = (ContentController)services.GetService(typeof(ContentController));
             _spritebatch = (SpriteBatch)services.GetService(typeof(SpriteBatch));
             _black = _content.GetContent<Texture2D>(@"StarFields\black");
@@ -57,19 +56,31 @@ namespace MasterOfCentauri.UIControls
         private void DrawBuildingCells()
         {
             var cells = ViewData.BuildingCells;
+            var offset = CalculateBuildingCellOffset(cells);
             for (int x = 0; x <= cells.GetUpperBound(0) ; x++)
             {
                 for (int y = 0; y <= cells.GetUpperBound(1); y++)
                 {
-                    Vector2 cellPosition = GetCellPosition(x,y);
+                    Vector2 cellPosition = GetCellPosition(offset,x,y);
                     _spritebatch.Draw(_testTexture, cellPosition, Color.White);
                 }
             }
         }
 
-        private Vector2 GetCellPosition(int x, int y)
+        private Vector2 CalculateBuildingCellOffset(BuildingCellViewModel[,] cells)
         {
-            return new Vector2(128,128) + new Vector2(x, y)*128;
+            var totalWidth = (cells.GetUpperBound(0)+1) * buildingCellSize;
+            var totalHeight = (cells.GetUpperBound(1)+1) * buildingCellSize;
+            var centerPoint = new Rectangle(0,0, (int)ActualWidth, (int)ActualHeight).Center;
+            
+            var offset = new Vector2(centerPoint.X, centerPoint.Y) - (new Vector2(totalWidth, totalHeight)/2);
+
+            return offset;
+        }
+
+        private Vector2 GetCellPosition(Vector2 offset, int x, int y)
+        {
+            return offset + new Vector2(x, y) * buildingCellSize;
         }
 
         protected override void OnUnload()
